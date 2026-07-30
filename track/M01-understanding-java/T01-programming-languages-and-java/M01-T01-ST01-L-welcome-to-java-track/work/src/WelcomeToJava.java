68634f5a7d3cc580e9579b2473aefd8bde45ebb1
Nizam Shaik <shaiknizam399@gmail.com>
2026-07-30 12:59:54 +0530

    Added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
         System.out.println("Welcome to the Java Track");
         System.out.println("I already know how to build programming logic");
         System.out.println("Now i am learning how to express that logic using java.");
+        System.out.println("Now i know how to use git and github");
     }
 }
