885c85bc0d5fe464352256e3d6fc871c58715e42
Nizam Shaik <shaiknizam399@gmail.com>
2026-08-05 23:00:09 +0530

    Added Displaying Git Repo status program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/DisplayGitRepo.java
@@ -0,0 +1,9 @@
+public class DisplayGitRepo {
+    public static void main(String[] args) {
+        // Display the repo cloning status.
+        System.out.println("Repository: Cloned Successfully");
+
+        // Display whether the project is ready.
+        System.out.println("Project status: Ready for Development");
+    }
+}
