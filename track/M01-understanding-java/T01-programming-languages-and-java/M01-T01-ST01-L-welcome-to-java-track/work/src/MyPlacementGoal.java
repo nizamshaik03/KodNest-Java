3474c6c985e08115e6ab76c2d9193f57415e0add
Nizam Shaik <shaiknizam399@gmail.com>
2026-08-05 22:52:06 +0530

    Added My placement Goal program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/MyPlacementGoal.java
@@ -0,0 +1,6 @@
+public class MyPlacementGoal {
+    public static void main(String[] args) {
+        System.out.println(
+                "--- MY PLACEMENT GOAL ---\nRole : Software Developer\nTarget : Get Placed\nDaily Target : 8 Hours");
+    }
+}
