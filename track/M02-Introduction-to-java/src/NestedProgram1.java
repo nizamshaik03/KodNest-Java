dad4456f281fe3f623d268ee4ad9241d6fea6c88
Nizam Shaik <shaiknizam399@gmail.com>
2026-08-06 12:47:12 +0530

    Added Nested program to print the i and j vlaues using for loop and break

---
+++ track/M02-Introduction-to-java/src/NestedProgram1.java
@@ -0,0 +1,12 @@
+public class NestedProgram1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 2; j++) {
+                if (j == 2) {
+                    break;
+                }
+                System.out.println("i : " + i + " j : " + j);
+            }
+        }
+    }
+}
