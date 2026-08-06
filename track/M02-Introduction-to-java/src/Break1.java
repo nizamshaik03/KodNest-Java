36dafb3fc9fe768bb1436a6d04b480747e1b8bfb
Nizam Shaik <shaiknizam399@gmail.com>
2026-08-06 12:25:44 +0530

    Added the Break - 1 program

---
+++ track/M02-Introduction-to-java/src/Break1.java
@@ -0,0 +1,13 @@
+public class Break1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 6) {
+                break;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
